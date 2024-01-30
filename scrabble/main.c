1b29a4cab6dac90c5a72d9487a4d9a5851092ce9
KrutikChaudhary <krutikchaudhary39@gmail.com>
2024-01-30 12:47:00 -0400

    problem 1 solution

--- scrabble/main.c
+++ scrabble/main.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main() {
-    printf("Hello, World\n");
+    //board size input
+    int n;
+    scanf("%d",&n);
+
+    //declare board
+    char board[n][n];
+
+    //initialize board
+    for(int i = 0; i < n ; i++){
+        for (int j = 0; j < n ; j++){
+            board[i][j]='.';
+        }
+    }
+    //number of words
+    int words;
+    scanf("%d",&words);
+
+    for(int i = 0; i < words; i++){
+        //initialize and scan words input
+        int x; int y;
+        char d;
+        char s[50];
+        scanf("%d %d %c %s",&x,&y,&d,s);
+
+        //if horizontal word placement
+        if(d=='H'){
+            int charCount = 0;
+            //move horizontally and place each character separately
+            for(int j = x; j<x+strlen(s); j++){
+                board[y][j]=s[charCount++];
+            }
+        }
+        //if vertical word placement
+        else if(d=='V'){
+            int charCount = 0;
+            //move vertically and place each character separately
+            for(int j = y; j<y+strlen(s); j++){
+                board[j][x]=s[charCount++];
+            }
+        }
+    }
+    int f;
+    scanf("%d", &f);
+
+    //output
+    for(int i = 0; i < n ; i++){
+        for (int j = 0; j < n ; j++){
+            printf("%c", board[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
